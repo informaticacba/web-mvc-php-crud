52c759444af76a72ea06dcc0ff1769471b1ded57
Alejandro Jesus del Campillo Jaime <alejandrodelcampillo@gmail.com>
2023-03-16 22:05:44 -0300

    Initial commit

---
+++ src/Tqdev/PhpCrudApi/OpenApi/OpenApiService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tqdev\PhpCrudApi\OpenApi;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Tqdev\PhpCrudApi\Column\ReflectionService;
+use Tqdev\PhpCrudApi\OpenApi\OpenApiBuilder;
+
+class OpenApiService
+{
+    private $builder;
+
+    public function __construct(ReflectionService $reflection, array $base, array $controllers, array $customBuilders)
+    {
+        $this->builder = new OpenApiBuilder($reflection, $base, $controllers, $customBuilders);
+    }
+
+    public function get(ServerRequestInterface $request): OpenApiDefinition
+    {
+        return $this->builder->build($request);
+    }
+}
